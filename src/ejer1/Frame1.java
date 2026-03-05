f73d6f74b92c57e0b5d03b24a94faa7d381cd5c8
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2026-03-05 16:31:42 +0100

    ej1 finished

--- src/ejer1/Frame1.java
+++ src/ejer1/Frame1.java
@@ -7,7 +7,14 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+import javax.swing.border.EtchedBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 public class Frame1 extends JFrame {
 
@@ -37,30 +44,67 @@ public class Frame1 extends JFrame {
 	 */
 	public Frame1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 406, 274);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(9, 242, 155));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel.setBackground(new Color(124, 250, 203));
+		sl_contentPane.putConstraint(SpringLayout.NORTH, panel, 0, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, panel, 10, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, panel, 143, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, panel, 200, SpringLayout.WEST, contentPane);
+		contentPane.add(panel);
+		SpringLayout sl_panel = new SpringLayout();
+		panel.setLayout(sl_panel);
 		
 		JLabel lblUser = new JLabel("user:");
-		lblUser.setBounds(10, 38, 46, 14);
-		contentPane.add(lblUser);
+		panel.add(lblUser);
 		
 		JLabel lblPassword = new JLabel("password:");
-		lblPassword.setBounds(10, 84, 57, 14);
-		contentPane.add(lblPassword);
-		
-		tf_user = new JTextField();
-		tf_user.setBounds(84, 35, 86, 20);
-		contentPane.add(tf_user);
-		tf_user.setColumns(10);
+		sl_panel.putConstraint(SpringLayout.NORTH, lblPassword, 52, SpringLayout.NORTH, panel);
+		sl_panel.putConstraint(SpringLayout.WEST, lblUser, 0, SpringLayout.WEST, lblPassword);
+		sl_panel.putConstraint(SpringLayout.SOUTH, lblUser, -18, SpringLayout.NORTH, lblPassword);
+		sl_panel.putConstraint(SpringLayout.WEST, lblPassword, 10, SpringLayout.WEST, panel);
+		lblPassword.setEnabled(false);
+		panel.add(lblPassword);
 		
 		tf_password = new JTextField();
-		tf_password.setBounds(84, 81, 86, 20);
-		contentPane.add(tf_password);
+		sl_panel.putConstraint(SpringLayout.NORTH, tf_password, -3, SpringLayout.NORTH, lblPassword);
+		sl_panel.putConstraint(SpringLayout.WEST, tf_password, 2, SpringLayout.EAST, lblPassword);
+		panel.add(tf_password);
 		tf_password.setColumns(10);
+		
+		tf_user = new JTextField();
+		sl_panel.putConstraint(SpringLayout.NORTH, tf_user, -3, SpringLayout.NORTH, lblUser);
+		sl_panel.putConstraint(SpringLayout.WEST, tf_user, 0, SpringLayout.WEST, tf_password);
+		panel.add(tf_user);
+		tf_user.setColumns(10);
+		
+		JButton btnCheck = new JButton("Check");
+		btnCheck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent eCheck) {
+				String typedUser= tf_user.getText();
+				String typedPass= tf_password.getText();
+				checkCredentials(eCheck, typedUser, typedPass);
+				
+			}
+		});
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnCheck, 27, SpringLayout.SOUTH, panel);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnCheck, 0, SpringLayout.EAST, panel);
+		contentPane.add(btnCheck);
 
+	}
+	
+	private void checkCredentials(ActionEvent event, String name, String keyWord) {
+		if (name.equals("admin") && keyWord.equals("admin")) {
+			JOptionPane.showMessageDialog(null, "Bienvenido");
+		}
+		else {JOptionPane.showMessageDialog(null, "User o password desconocido"); }
 	}
 }
